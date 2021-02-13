6f76d7508e94234013a6eb66ce5bf953485dde78
Aeron01 <one.soulreaper@gmail.com>
2021-02-13 17:24:27 +0100

    test

---
+++ trest.js
@@ -0,0 +1,14 @@
+let age = 1200
+if (age < 18) {
+  console.log('Vous êtes mineur.')
+} else if (age <= 65) {
+  console.log('Vous êtes majeur.')
+} else if (age <= 75) {
+  console.log('Vous êtes sénior')
+} else if (age <= 100) {
+  console.log('Vous êtes centenaire.')
+} else if (age < 125) {
+  console.log('Vous faite partie des personnes les plus agées de l Humanité.')
+} else {
+  console.log('Les morts ne parles pas !')
+}
